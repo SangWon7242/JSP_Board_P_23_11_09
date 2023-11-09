72d12ea9c840aa8854c36bcd14d307dd6c9ce3bb
SangWon7242 <gotkdqja@gmail.com>
2023-11-09 20:59:08 +0900

    req와 resp를 직접 다루기 귀찮아서 Rq클래스 도입

---
+++ src/main/java/sbs/jsp/board/Rq.java
@@ -0,0 +1,58 @@
+package sbs.jsp.board;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class Rq {
+  private final HttpServletRequest req;
+  private final HttpServletResponse resp;
+
+  public Rq(HttpServletRequest req, HttpServletResponse resp) {
+    this.req = req;
+    this.resp = resp;
+
+    try {
+      req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 해석하겠다.
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    }
+    resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML 인코딩을 UTF-8로 하겠다.
+    resp.setContentType("text/html; charset-ut-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이다 라고 알리는 의미
+  }
+
+  public int getIntParam(String paramName, int defaultValue) {
+    String value = req.getParameter(paramName);
+
+    if(value == null) {
+      return defaultValue;
+    }
+
+    try {
+      return Integer.parseInt(value);
+    }
+    catch (NullPointerException e) {
+      return defaultValue;
+    }
+  }
+
+  public String getParam(String paramName, String defaultValue) {
+    String value = req.getParameter(paramName);
+
+    if (value == null) {
+      return defaultValue;
+    }
+
+    return value;
+  }
+
+  public void appendBody(String str) {
+    try {
+      resp.getWriter().append(str);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
--- src/main/java/sbs/jsp/board/servlet/GugudanServlet.java
+++ src/main/java/sbs/jsp/board/servlet/GugudanServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import sbs.jsp.board.Rq;
 
 import java.io.IOException;
 
@@ -12,17 +13,15 @@ import java.io.IOException;
 public class GugudanServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 해석하겠다.
-    resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML 인코딩을 UTF-8로 하겠다.
-    resp.setContentType("text/html; charset-ut-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이다 라고 알리는 의미
+    Rq rq = new Rq(req, resp);
 
-    int dan = Integer.parseInt(req.getParameter("dan"));
-    int limit = Integer.parseInt(req.getParameter("limit"));
+    int dan = rq.getIntParam("dan", 9);
+    int limit = rq.getIntParam("limit", 9);
 
-    resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+    rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
 
     for(int i = 1; i <= limit; i++) {
-      resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+      rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
     }
   }
 }
