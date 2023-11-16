ffc1c6699e28acdee8f3fbfdaf1ffbbe0a22960a
SangWon7242 <gotkdqja@gmail.com>
2023-11-16 19:56:27 +0900

    게시물 상세 페이지 구현

---
+++ src/main/java/sbs/jsp/board/servlet/UsrArticleDetailServlet.java
@@ -0,0 +1,47 @@
+package sbs.jsp.board.servlet;
+
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import sbs.jsp.board.Rq;
+import sbs.jsp.board.util.MysqlUtil;
+import sbs.jsp.board.util.SecSql;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet("/usr/article/detail")
+public class UsrArticleDetailServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "jspboard");
+    MysqlUtil.setDevMode(true);
+
+    Rq rq = new Rq(req, resp);
+
+    int id = rq.getIntParam("id", 0);
+
+    if(id == 0) {
+      rq.appendBody("<script>alert('잘못 된 요청입니다.'); history.back(); </script>");
+      return;
+    }
+
+    SecSql sql = new SecSql();
+    sql.append("SELECT A.*");
+    sql.append("FROM article AS A");
+    sql.append("WHERE A.id = ?", id);
+
+    Map<String, Object> articleRow = MysqlUtil.selectRow(sql);
+
+    rq.setAttr("articleRow", articleRow);
+
+    RequestDispatcher requestDispatcher = req.getRequestDispatcher("/article/detail.jsp");
+    requestDispatcher.forward(req, resp);
+
+  }
+}
