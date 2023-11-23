1619359362f2c3348ab481913d9f91ce32c6898c
SangWon7242 <gotkdqja@gmail.com>
2023-11-23 19:28:11 +0900

    게시물 작성시 로그인 한 회원의 번호가 article 테이블에 같이 저장되도록 작업

--- DB/schema.sql
+++ DB/schema.sql
@@ -33,6 +33,7 @@ updateDate = NOW(),
 title = '제목3',
 content = '내용3';
 
+
 # 랜덤하게 테스트 데이터 생성
 INSERT INTO article(regDate, updateDate, title, content)
 SELECT NOW(), NOW(), CONCAT('제목-', FLOOR(RAND() * 100)), CONCAT('내용-', FLOOR(RAND() * 100))
@@ -48,3 +49,44 @@ CREATE TABLE MEMBER(
 	`name` CHAR(100) NOT NULL,
 	email CHAR(100) NOT NULL UNIQUE
 );
+
+# 회원 테이블 데이터 생성
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'admin',
+loginPw = '1234',
+`name` = '관리자',
+email = 'admin@test.com';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'user1',
+loginPw = '1234',
+`name` = '유저1',
+email = 'user1@test.com';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'user2',
+loginPw = '1234',
+`name` = '유저2',
+email = 'user2@test.com';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'user3',
+loginPw = '1234',
+`name` = '유저3',
+email = 'user3@test.com';
+
+# 게시물 테이블에 memberId 칼럼 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER regDate;
+
+# 기존 게시물은 그냥 2번 회원이 전부 작성한걸로 정한다.
+UPDATE article
+SET memberId = 2
+WHERE memberId = 0;
